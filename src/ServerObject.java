5ead94d50b82ab7dc94d0f080ccd44f232ac7fd0
Thibault <thibault.morisse@etu.enseeiht.fr>
2012-03-29 17:03:33 +0200

    ajout serverObject²

--- src/ServerObject.java
+++ src/ServerObject.java
@@ -7,11 +7,14 @@ import java.util.concurrent.locks.ReentrantLock;
 
 public class ServerObject {
 	
+	// 1 Shared object avec un numéro
+	// clients qui s'adressent à lui
+	
 	// Attributes
 	private int id ;
 	private Object object ;
-	private ArrayList<Client> listReaders = new ArrayList<Client>() ;
-	private Client writer ;
+	private ArrayList<Client_itf> listReaders = new ArrayList<Client_itf>() ;
+	private Client_itf writer ;
 	private final Lock mutexMonitor = new ReentrantLock();
 	
 	// Constructor
@@ -22,34 +25,60 @@ public class ServerObject {
 	
 	
 	// Writer exists method returns a boolean
-	public boolean exists(Client writer) {
-		return (writer!=null);		
+	public boolean writerExists() {
+		return (this.writer!=null);		
+	}
+	
+	// Readers exist method returns a boolean
+	public boolean readersExist() {
+		return (this.listReaders.size()>0);
+	}
+	
+	// LOCK_WRITE
+	public Object lock_write(Client_itf client) throws RemoteException {
+				
+		Object obj ;
+		
+		mutexMonitor.lock();
+		
+		if (!writerExists() && !readersExist()) {
+			obj = this.object;
+		}
+		else
+		{
+			if (writerExists()) {
+				this.object = this.writer.invalidate_writer(this.id);
+				// writer == false
+			}
+			for(Client_itf reader : this.listReaders) {
+				reader.invalidate_reader(this.id);
+			}
+			listReaders.clear();
+			obj = this.object;
+		}
+		this.writer = client;
+				
+		mutexMonitor.unlock();
+		
+		return obj;		
 	}
 	
 	
-	public void lock_write(Client client) {
+	// LOCK_READ
+	public Object lock_read(Client_itf client) throws RemoteException {
+		
 		mutexMonitor.lock();
 		
-		if (exists(this.writer)) {
-			try {
-				this.writer.invalidate_writer(this.id);
-			} catch (RemoteException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			// writer == false
-			
-			
+		if (writerExists()) {
+			this.object = this.writer.reduce_lock(this.id);
+			this.writer = null ;
 		}
 		
-		mutexMonitor.unlock();		
-	}
-	
-	public void lock_read(Client client) {
-		mutexMonitor.lock();
-		
+		this.listReaders.add(client);
 		
 		mutexMonitor.unlock();
+		
+		return this.object;
 	}
 	
 }
